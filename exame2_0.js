af299c03a2871851ea598558ddf973dc199b3c77
Aller010 <145874641+Aller010@users.noreply.github.com>
2024-02-18 01:54:49 +0400

    Correct files for exame

---
+++ exame2_0.js
@@ -0,0 +1,113 @@
+const taskList = new TaskList();
+taskList.loadTasks();
+
+const taskForm = document.getElementById('task-form');
+const taskNameInput = document.getElementById('task-name');
+const taskDescriptionInput = document.getElementById('task-description');
+const tasksList = document.getElementById('tasks-list');
+const sortSelect = document.getElementById('sort-select');
+const filterSelect = document.getElementById('filter-select');
+
+function isValid(value) {
+    return /^(?!\s*)[ER]{2,16}\s[ER]{2,16}/.test(value);
+}
+
+function isValidDescription(description) {
+    return /^(?!\s*)[EN]{1,16}/.test(description);
+}
+
+function validateForm() {
+    const name = taskNameInput.value;
+    const description = taskDescriptionInput.value;
+
+    if (name.length === 0 || description.length === 0) {
+        alert('Invalid input');
+        return false;
+    }
+    return true;
+}
+
+taskForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    if (!validateForm()) {
+        return;
+    }
+
+    const name = taskNameInput.value.trim();
+    const description = taskDescriptionInput.value.trim();
+    const date = new Date().toLocaleString();
+    const completed = false;
+
+    const task = new Task(name, description, date, completed);
+
+    taskList.addTask(task);
+
+    taskList.sortTasks(sortSelect.value);
+    renderTasks(taskList.filterTasks(filterSelect.value));
+    taskForm.reset();
+});
+
+function renderTasks(tasks) {
+    tasksList.innerHTML = '';
+    tasks.forEach(task => {
+        const taskElement = document.createElement('div');
+        taskElement.classList.add('task');
+
+        const checkbox = document.createElement('input');
+        checkbox.type = 'checkbox';
+        checkbox.checked = task.completed;
+        checkbox.addEventListener('change', () => {
+            task.completed = checkbox.checked;
+            taskList.editTask(task.id, task);
+            renderTasks(taskList.filterTasks(filterSelect.value));
+        });
+
+        const nameElement = document.createElement('h2');
+        nameElement.textContent = task.name;
+        nameElement.addEventListener('click', () => {
+            window.location.href = 'Details.html?id=' + task.id;
+        });
+
+
+        const deleteButton = document.createElement('button');
+        deleteButton.textContent = 'Delete';
+        deleteButton.addEventListener('click', () => {
+            taskList.deleteTask(task.id);
+            taskList.sortTasks(sortSelect.value);
+            renderTasks(taskList.filterTasks(filterSelect.value));
+        });
+
+        const editButton = document.createElement('button');
+        editButton.textContent = 'Edit';
+        editButton.addEventListener('click', () => {
+            window.location.href = 'Edit.html?id=' + task.id;
+        });
+
+        taskElement.appendChild(checkbox);
+        taskElement.appendChild(nameElement);
+        taskElement.appendChild(deleteButton);
+        taskElement.appendChild(editButton);
+        tasksList.appendChild(taskElement);
+    });
+}
+
+function filterTasks(filter) {
+    const filteredTasks = taskList.filterTasks(filter);
+    renderTasks(filteredTasks);
+}
+
+function sortTasks(sortBy) {
+    taskList.sortTasks(sortBy);
+    renderTasks(taskList.filterTasks(filterSelect.value));
+}
+
+sortSelect.addEventListener('change', () => {
+    sortTasks(sortSelect.value);
+});
+
+filterSelect.addEventListener('change', () => {
+    filterTasks(filterSelect.value);
+});
+
+renderTasks(taskList.filterTasks("Date"));
---
+++ functional.js
@@ -0,0 +1,91 @@
+class Task {
+    #id;
+    #name;
+    #description;
+    #date;
+    #completed;
+
+    constructor(name, description, date, completed) {
+        this.#id = crypto.randomUUID();
+        this.#name = name;
+        this.#description = description;
+        this.#date = date;
+        this.#completed = completed;
+    }
+
+    get id() { return this.#id; }
+    get name() {
+        return this.#name;
+    }
+    set name(value) { this.#name = value.trim(); }
+    get description() { return this.#description; }
+    set description(value) { this.#description = value.trim(); }
+    get date() { return this.#date; }
+    get completed() { return this.#completed; }
+    set completed(value) { this.#completed = value; }
+
+    toJSON() {
+        return {
+            id: this.#id,
+            name: this.#name,
+            description: this.#description,
+            date: this.#date,
+            completed: this.#completed
+        };
+    }
+}
+
+class TaskList {
+    #tasks = [];
+
+    addTask(task) {
+        this.#tasks.push(task);
+        this.saveTasks();
+    }
+
+    editTask(id, updatedTask) {
+        const taskIndex = this.#tasks.findIndex(task => task.id === id);
+        if (taskIndex !== -1) {
+            this.#tasks[taskIndex] = updatedTask;
+            this.saveTasks();
+        }
+    }
+
+    deleteTask(id) {
+        this.#tasks = this.#tasks.filter(task => task.id !== id);
+        this.saveTasks();
+    }
+
+    getTasks() {
+        return this.#tasks;
+    }
+
+    filterTasks(filter) {
+        return this.#tasks.filter(task => {
+            if (filter === 'completed') {
+                return task.completed === true;
+            } else if (filter === 'uncompleted') {
+                return task.completed === false;
+            } else {
+                return true;
+            }
+        });
+    }
+
+    sortTasks(sortBy) {
+        if (sortBy === 'name') {
+            this.#tasks.sort((a, b) => a.name.localeCompare(b.name));
+        } else if (sortBy === 'date') {
+            this.#tasks.sort((a, b) => new Date(b.date) - new Date(a.date));
+        }
+    }
+
+    saveTasks() {
+        localStorage.setItem('tasks', JSON.stringify(this.#tasks));
+    }
+
+    loadTasks() {
+        this.#tasks = JSON.parse(localStorage.getItem('tasks')) || [];
+    }
+}
+
